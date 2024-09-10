a13888262f42f8389add642d62cad93ea812c341
Nurbek Abdirasulov <nurbekabdirasulov0@gmail.com>
2024-09-10 12:46:48 -0500

    add logger

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM public.ecr.aws/docker/library/openjdk:17 AS build
 WORKDIR /app
 COPY . .
 RUN ./mvnw clean package
+
 FROM public.ecr.aws/docker/library/openjdk:17.0.2-jdk-slim
 WORKDIR /app
 COPY --from=build /app/target/Codewise-0.0.1-SNAPSHOT.jar ./codewise.jar
--- src/main/java/com/codewise/CodewiseApplication.java
+++ src/main/java/com/codewise/CodewiseApplication.java
@@ -3,11 +3,17 @@ package com.codewise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CodewiseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodewiseApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(CodewiseApplication.class.getName());
+        SpringApplication.run(CodewiseApplication.class, args);
+
+        logger.info("Starting the application!!");
+
+    }
 }
