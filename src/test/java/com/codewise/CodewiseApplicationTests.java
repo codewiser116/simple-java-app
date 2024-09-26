ae18c1ee4343a55a160b69872a418cdb59052b98
codewiser116 <codewise.academy@gmail.com>
2024-09-26 18:57:48 -0500

    added failed tests

--- src/test/java/com/codewise/CodewiseApplicationTests.java
+++ src/test/java/com/codewise/CodewiseApplicationTests.java
@@ -2,12 +2,14 @@ package com.codewise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class CodewiseApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(4 < 2, "message");
 	}
 
 }
